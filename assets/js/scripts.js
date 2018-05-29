feb32f338660272af8927f551f850c33aec1564c
Dave <drregg6@gmail.com>
2018-05-28 21:52:33 -0400

    adding a couple of buttons

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,36 +1,39 @@
 let primaryMin = document.querySelector('.primary-min');
 let primarySec = document.querySelector('.primary-sec');
+let startButton = document.querySelector('.start');
+let pauseButton = document.querySelector('.pause');
 let inputTime = parseInt(primaryMin.innerText);
 let totalSeconds = inputTime * 60;
-let count = 0;
 
-let intervalId = setInterval(function() {
-    let min = Math.floor(totalSeconds / 60);
-    let sec = totalSeconds % 60;
+startButton.addEventListener('click', function() {
+    let totalSeconds = inputTime * 60;
 
-    if (min === 0) {
-        primaryMin.textContent = '0';
-    } else {
-        primaryMin.textContent = '' + min;
-    }
+    let intervalId = setInterval(function() {
+        let min = Math.floor(totalSeconds / 60);
+        let sec = totalSeconds % 60;
 
-    if (sec >= 0 && sec <= 9) {
-        primarySec.textContent = '0' + sec;
-    } else {
-        primarySec.textContent = '' + sec;
-    }
+        if (min === 0) {
+            primaryMin.textContent = '0';
+        } else {
+            primaryMin.textContent = '' + min;
+        }
 
-    totalSeconds--;
+        if (sec >= 0 && sec <= 9) {
+            primarySec.textContent = '0' + sec;
+        } else {
+            primarySec.textContent = '' + sec;
+        }
 
-    if (totalSeconds === -1) {
-        primaryMin.textContent = '0';
-        primarySec.textContent = '00';
-        clearInterval(intervalId);
-    }
+        totalSeconds--;
 
-    // count++;
-    // if (count === 3) {
-    //     clearInterval(intervalId);
-    // }
-    // console.log(totalSeconds);
-}, 1000)
+        if (totalSeconds === -1) {
+            primaryMin.textContent = '0';
+            primarySec.textContent = '00';
+            clearInterval(intervalId);
+        }
+    }, 1000)
+});
+
+// pauseButton.addEventListener('click', function() {
+//     clearInterval(intervalId);
+// });
